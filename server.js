333e9eaaa474ca281df12f2b15545457b37ea1aa
Digvijay Sanjay Ingole <64873101+DigvijayIngole55@users.noreply.github.com>
2024-07-21 02:20:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,27 @@ import imageRoutes from './image.js';
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+// CORS Configuration
+const corsOptions = {
+  origin: '*', // Allow all origins (adjust as needed)
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api', tesseractRoutes);
 app.use('/recipes', recipeRoutes);
 app.use('/images', imageRoutes);
 app.use('/uploads', express.static('uploads')); // Serve static files from uploads folder
+
+// Basic logging middleware to see incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
