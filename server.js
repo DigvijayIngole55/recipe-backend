a1948baae037e24ca63c41797d7d41e6129ff84b
Digvijay Sanjay Ingole <64873101+DigvijayIngole55@users.noreply.github.com>
2024-07-21 02:36:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,18 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import tesseractRoutes from './Tesseract.js';
 import recipeRoutes from './recipes.js';
-import imageRoutes from './image.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', tesseractRoutes);
+
+app.use('/api', tesseractRoutes); // Ensure this matches the endpoint being called
 app.use('/recipes', recipeRoutes);
-app.use('/images', imageRoutes);
-app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
