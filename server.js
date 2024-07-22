a1c2d3c3ac2e6f64ea7811fdedad15953715f02c
Digvijay Sanjay Ingole <64873101+DigvijayIngole55@users.noreply.github.com>
2024-07-21 21:10:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.use('/api', tesseractRoutes);
 app.use('/recipes', recipeRoutes);
 app.use('/uploads', express.static('uploads')); // Serve static files from uploads folder
 
+const server = http.createServer(app);
+
+// Set timeout to 120 seconds (120000 milliseconds)
+server.setTimeout(120000);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
